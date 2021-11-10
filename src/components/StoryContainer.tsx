d39f9b020bfc38bbac28316013083015e56370ec
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-11-10 08:17:55 +0000

    aligned story to start

--- src/components/StoryContainer.tsx
+++ src/components/StoryContainer.tsx
@@ -58,7 +58,9 @@ export default function StoryContainer({ story }: StoryContainerProps) {
         {story?.storyImage && <Image src={story?.storyImage} fluid rounded />}
       </div>
       {story?.story && (
-        <div className="mb-4">{ReactHtmlParser(story?.story)}</div>
+        <div className="mb-4 align-self-start">
+          {ReactHtmlParser(story?.story)}
+        </div>
       )}
       <div className="hearts-comments align-self-start mb-4">
         <span className="me-4" onClick={handleHeart}>
