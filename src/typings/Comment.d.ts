8abf28bd9c915739d3a12f562d4fdd27cc6b92f6
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-11-11 10:14:41 +0000

    Deleted typing for sub comments

--- src/typings/Comment.d.ts
+++ src/typings/Comment.d.ts
@@ -1,13 +1,10 @@
 import { Author } from "./Author";
-import { SubComment } from "./SubComment";
 
 export interface Comment {
   _id: string;
   story: string;
   author: Author;
   comment: string;
-  hearts: string[];
-  subComments: SubComment[];
   createdAt: string;
   updatedAt: string;
 }
