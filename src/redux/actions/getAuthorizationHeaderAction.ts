b9b237ad8b51ec9edc44c1767abc89389f513293
André Sousa <andrels9283@gmail.com>
2022-12-17 20:53:22 +0000

    format with Vs

--- src/redux/actions/getAuthorizationHeaderAction.ts
+++ src/redux/actions/getAuthorizationHeaderAction.ts
@@ -1,61 +1,61 @@
 import { Dispatch } from "redux";
-import { ReduxStore } from "../../typings/ReduxStore";
+import { ReduxStore } from "../../types/ReduxStore";
 import {
-  FILL_AUTHORIZATION_HEADER,
-  LOADING_AUTHORIZATION_HEADER,
-  ERROR_AUTHORIZATION_HEADER,
+    FILL_AUTHORIZATION_HEADER,
+    LOADING_AUTHORIZATION_HEADER,
+    ERROR_AUTHORIZATION_HEADER,
 } from "./actionTypes";
 import axios from "axios";
-import { LoginInfo } from "../../typings/LoginInfo";
+import { LoginInfo } from "../../types/LoginInfo";
 
 export const getAuthorizationHeaderAction = (
-  loginInfo: LoginInfo,
-  historyPush: (location: string) => void
+    loginInfo: LoginInfo,
+    historyPush: (location: string) => void
 ) => {
-  return async (dispatch: Dispatch, getState: () => ReduxStore) => {
-    try {
-      dispatch({
-        type: ERROR_AUTHORIZATION_HEADER,
-        payload: "",
-      });
-      dispatch({
-        type: LOADING_AUTHORIZATION_HEADER,
-        payload: true,
-      });
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/authors/login`,
-        loginInfo
-      );
+    return async (dispatch: Dispatch, getState: () => ReduxStore) => {
+        try {
+            dispatch({
+                type: ERROR_AUTHORIZATION_HEADER,
+                payload: "",
+            });
+            dispatch({
+                type: LOADING_AUTHORIZATION_HEADER,
+                payload: true,
+            });
+            const response = await axios.post(
+                `${process.env.REACT_APP_API_URL}/authors/login`,
+                loginInfo
+            );
 
-      if (response.status === 200) {
-        dispatch({
-          type: FILL_AUTHORIZATION_HEADER,
-          payload: response.data.accessToken,
-        });
-        dispatch({
-          type: LOADING_AUTHORIZATION_HEADER,
-          payload: false,
-        });
-        historyPush("/me");
-      } else {
-        dispatch({
-          type: ERROR_AUTHORIZATION_HEADER,
-          payload: response.data.message,
-        });
-        dispatch({
-          type: LOADING_AUTHORIZATION_HEADER,
-          payload: false,
-        });
-      }
-    } catch (error: any) {
-      dispatch({
-        type: ERROR_AUTHORIZATION_HEADER,
-        payload: error.message,
-      });
-      dispatch({
-        type: LOADING_AUTHORIZATION_HEADER,
-        payload: false,
-      });
-    }
-  };
+            if (response.status === 200) {
+                dispatch({
+                    type: FILL_AUTHORIZATION_HEADER,
+                    payload: response.data.accessToken,
+                });
+                dispatch({
+                    type: LOADING_AUTHORIZATION_HEADER,
+                    payload: false,
+                });
+                historyPush("/me");
+            } else {
+                dispatch({
+                    type: ERROR_AUTHORIZATION_HEADER,
+                    payload: response.data.message,
+                });
+                dispatch({
+                    type: LOADING_AUTHORIZATION_HEADER,
+                    payload: false,
+                });
+            }
+        } catch (error: any) {
+            dispatch({
+                type: ERROR_AUTHORIZATION_HEADER,
+                payload: error.message,
+            });
+            dispatch({
+                type: LOADING_AUTHORIZATION_HEADER,
+                payload: false,
+            });
+        }
+    };
 };
